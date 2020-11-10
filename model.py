96ebf4ecc78a621b17bab2e416063ae9d5012c07
camille <camille@geotrend.fr>
2020-11-10 16:40:34 +0100

    new model using exclusively attention between e1 and e2 at each layer

--- model.py
+++ model.py
@@ -25,53 +25,112 @@ class RBERT(BertPreTrainedModel):
 
         self.num_labels = config.num_labels
 
-        self.cls_fc_layer = FCLayer(config.hidden_size, config.hidden_size, args.dropout_rate)
-        self.entity_fc_layer = FCLayer(config.hidden_size, config.hidden_size, args.dropout_rate)
         self.label_classifier = FCLayer(
-            config.hidden_size * 3,
+            config.num_hidden_layers*config.num_attention_heads*2,
             config.num_labels,
             args.dropout_rate,
             use_activation=False,
         )
 
-    @staticmethod
-    def entity_average(hidden_output, e_mask):
-        """
-        Average the entity hidden state vectors (H_i ~ H_j)
-        :param hidden_output: [batch_size, j-i+1, dim]
-        :param e_mask: [batch_size, max_seq_len]
-                e.g. e_mask[0] == [0, 0, 0, 1, 1, 1, 0, 0, ... 0]
-        :return: [batch_size, dim]
-        """
-        e_mask_unsqueeze = e_mask.unsqueeze(1)  # [b, 1, j-i+1]
-        length_tensor = (e_mask != 0).sum(dim=1).unsqueeze(1)  # [batch_size, 1]
+    # @staticmethod
+    # def entity_average(hidden_output, e_mask):
+    #     """
+    #     Average the entity hidden state vectors (H_i ~ H_j)
+    #     :param hidden_output: [batch_size, j-i+1, dim]
+    #     :param e_mask: [batch_size, max_seq_len]
+    #             e.g. e_mask[0] == [0, 0, 0, 1, 1, 1, 0, 0, ... 0]
+    #     :return: [batch_size, dim]
+    #     """
+    #     e_mask_unsqueeze = e_mask.unsqueeze(1)  # [b, 1, j-i+1]
+    #     length_tensor = (e_mask != 0).sum(dim=1).unsqueeze(1)  # [batch_size, 1]
 
-        # [b, 1, j-i+1] * [b, j-i+1, dim] = [b, 1, dim] -> [b, dim]
-        sum_vector = torch.bmm(e_mask_unsqueeze.float(), hidden_output).squeeze(1)
-        avg_vector = sum_vector.float() / length_tensor.float()  # broadcasting
+    #     # [b, 1, j-i+1] * [b, j-i+1, dim] = [b, 1, dim] -> [b, dim]
+    #     sum_vector = torch.bmm(e_mask_unsqueeze.float(), hidden_output).squeeze(1)
+    #     avg_vector = sum_vector.float() / length_tensor.float()  # broadcasting
+    #     return avg_vector
+
+    @staticmethod
+    def attending_entity_average(attentions, e_mask):
+        """
+        Average the attention vectors (H_i ~ H_j)
+        :param attentions: [layers_count, batch_size, heads_count, max_seq_length, max_seq_length]
+        :param e_mask: [batch_size, max_seq_length]
+                e.g. e_mask[0] == [0, 0, 0, 1, 1, 1, 0, 0, ... 0]
+        :return: [layers_count, batch_size, heads_count, max_seq_length]
+        """
+        e_mask_unsqueeze = e_mask
+        e_mask_unsqueeze = e_mask_unsqueeze.unsqueeze(2)  # [batch_size, max_seq_length, 1]
+        e_mask_unsqueeze = e_mask_unsqueeze.unsqueeze(1)  # [batch_size, 1, max_seq_length, 1]
+        e_mask_unsqueeze = e_mask_unsqueeze.unsqueeze(0)  # [1, batch_size, 1, max_seq_length, 1]
+        length_tensor = (e_mask != 0).sum(dim=1).unsqueeze(1).unsqueeze(1).unsqueeze(0)  # [1, batch_size, 1, 1]
+
+        mul_vector = e_mask_unsqueeze.float() * attentions # [layers_count, batch_size, heads_count, max_seq_length, max_seq_length]
+        sum_vector = torch.sum(mul_vector, dim=3) # [layers_count, batch_size, heads_count, max_seq_length]
+        avg_vector = sum_vector.float() / length_tensor.float() # [layers_count, batch_size, heads_count, max_seq_length]
         return avg_vector
+
+    @staticmethod
+    def attended_entity_average(attentions, e_mask):
+        """
+        Average the attention vectors (H_i ~ H_j)
+        :param attentions: [layers_count, batch_size, heads_count, max_seq_length]
+        :param e_mask: [batch_size, max_seq_length]
+                e.g. e_mask[0] == [0, 0, 0, 1, 1, 1, 0, 0, ... 0]
+        :return: [layers_count, batch_size, heads_count]
+        """
+        e_mask_unsqueeze = e_mask
+        e_mask_unsqueeze = e_mask_unsqueeze.unsqueeze(1)  # [batch_size, 1, max_seq_length]
+        e_mask_unsqueeze = e_mask_unsqueeze.unsqueeze(0)  # [1, batch_size, 1, max_seq_length]
+        length_tensor = (e_mask != 0).sum(dim=1).unsqueeze(1).unsqueeze(0)  # [1, batch_size, 1, 1]
+
+        mul_vector = e_mask_unsqueeze.float() * attentions # [layers_count, batch_size, heads_count, max_seq_length]
+        sum_vector = torch.sum(mul_vector, dim=3) # [layers_count, batch_size, heads_count]
+        avg_vector = sum_vector.float() / length_tensor.float() # [layers_count, batch_size, heads_count]
+        return avg_vector
+
+    @staticmethod
+    def entity_to_entity_attentions(attentions, e1_mask, e2_mask):
+        """
+        Select the attention heads from e1 to e2 averaging over all tokens for each entity.
+        :param attentions: [layers_count, batch_size, heads_count, max_seq_length, max_seq_length]
+        :param e1_mask: [batch_size, max_seq_length]
+                e.g. e_mask[0] == [0, 0, 0, 1, 1, 1, 0, 0, ... 0]
+        :param e2_mask: [batch_size, max_seq_length]
+                e.g. e_mask[0] == [0, 0, 0, 1, 1, 1, 0, 0, ... 0]
+        :return: [layers_count, batch_size, heads_count]
+        """
+        e1_avg_vector = RBERT.attending_entity_average(attentions, e1_mask)
+        e2_avg_vector = RBERT.attended_entity_average(e1_avg_vector, e2_mask)
+        return e2_avg_vector
 
     def forward(self, input_ids, attention_mask, token_type_ids, labels, e1_mask, e2_mask):
         outputs = self.bert(
-            input_ids, attention_mask=attention_mask, token_type_ids=token_type_ids
+            input_ids, attention_mask=attention_mask, token_type_ids=token_type_ids,
+            return_dict=True, output_attentions=True
         )  # sequence_output, pooled_output, (hidden_states), (attentions)
-        sequence_output = outputs[0]
-        pooled_output = outputs[1]  # [CLS]
+
+
+        attentions = outputs['attentions'] # tuple of layers_count tensors
+                                           # each tensor is of shape batch_size, heads_count, max_seq_length, max_seq_length
+        attentions = torch.stack(attentions) # layers_count, batch_size, heads_count, max_seq_length, max_seq_length
 
         # Average
-        e1_h = self.entity_average(sequence_output, e1_mask)
-        e2_h = self.entity_average(sequence_output, e2_mask)
+        e1_to_e2_attentions = self.entity_to_entity_attentions(attentions, e1_mask, e2_mask) # layers_count, batch_size, heads_count
+        e2_to_e1_attentions = self.entity_to_entity_attentions(attentions, e2_mask, e1_mask) # layers_count, batch_size, heads_count
 
-        # Dropout -> tanh -> fc_layer (Share FC layer for e1 and e2)
-        pooled_output = self.cls_fc_layer(pooled_output)
-        e1_h = self.entity_fc_layer(e1_h)
-        e2_h = self.entity_fc_layer(e2_h)
+        # swap dimensions (we could also use transpose)
+        e1_to_e2_attentions = e1_to_e2_attentions.permute(1, 0, 2) # batch_size, layers_count, heads_count
+        e2_to_e1_attentions = e2_to_e1_attentions.permute(1, 0, 2) # batch_size, layers_count, heads_count
 
-        # Concat -> fc_layer
-        concat_h = torch.cat([pooled_output, e1_h, e2_h], dim=-1)
-        logits = self.label_classifier(concat_h)
+        # flatten and concat attentions
+        e1_to_e2_attentions = e1_to_e2_attentions.reshape(e1_to_e2_attentions.shape[0], -1) # batch_size, layers_count*heads_count
+        e2_to_e1_attentions = e2_to_e1_attentions.reshape(e2_to_e1_attentions.shape[0], -1) # batch_size, layers_count*heads_count
+        e_to_e_attentions = torch.cat((e1_to_e2_attentions, e2_to_e1_attentions), 1) # batch_size, layers_count*heads_count*2
 
-        outputs = (logits,) + outputs[2:]  # add hidden states and attention if they are here
+        # fc classifier
+        logits = self.label_classifier(e_to_e_attentions)
+
+        outputs = (logits,)
 
         # Softmax
         if labels is not None:
@@ -84,4 +143,4 @@ class RBERT(BertPreTrainedModel):
 
             outputs = (loss,) + outputs
 
-        return outputs  # (loss), logits, (hidden_states), (attentions)
+        return outputs  # (loss), logits
